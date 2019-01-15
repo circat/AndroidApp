a28a6b5a1b8680377f3150ff5815048c4a8e4c8f
circat <erichlesovsky@gmail.com>
2019-01-16 00:05:41 +0100

    First commit

---
+++ app/src/main/java/com/app/geoloc/geo1/MainActivity.java
@@ -0,0 +1,295 @@
+package geo11.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.hardware.SensorEvent;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.hardware.SensorEventListener;
+import android.os.Bundle;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.os.IBinder;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+import android.Manifest;
+import android.location.Location;
+import android.app.Notification;
+import android.content.pm.PackageManager;
+import android.app.PendingIntent;
+import android.app.Service;
+
+
+public class MainActivity extends Activity implements SensorEventListener {
+    // device sensor manager
+    private SensorManager SensorManage;
+
+    // define the compass picture that will be use
+    private ImageView compassimage;
+    // record the angle turned of the compass picture
+    private float DegreeStart = 0f;
+
+    TextView DegreeTV;
+
+    private static final int PERMISSIONS_REQUEST = 100;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        compassimage = (ImageView) findViewById(R.id.compass_image);
+
+        // TextView that will display the degree
+        DegreeTV = (TextView) findViewById(R.id.DegreeTV);
+
+        // initialize your android device sensor capabilities
+        SensorManage = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        buildNotification();
+        loginToFirebase();
+
+        //Check whether GPS tracking is enabled//
+
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            finish();
+        }
+
+        //Check whether this app has access to the location permission//
+
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        //If the location permission has been granted, then start the TrackerService//
+
+
+    }
+//Create the persistent notification//
+
+    private void buildNotification() {
+        String stop = "stop";
+        registerReceiver(stopReceiver, new IntentFilter(stop));
+        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
+                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
+
+/*// Create the persistent notification//
+        Notification notification = new Notification.Builder(Context)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.tracking_enabled_notif));
+*/
+    }
+
+    protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+//Unregister the BroadcastReceiver when the notification is tapped//
+
+            unregisterReceiver(stopReceiver);
+
+//Stop the Service//
+
+         //   stopSelf();
+        }
+    };
+
+    private void loginToFirebase() {
+
+//Authenticate with Firebase, using the email and password we created earlier//
+
+        String email = getString(R.string.test_email);
+        String password = getString(R.string.test_password);
+
+//Call OnCompleteListener if the user is signed in successfully//
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+
+//If the user has been authenticated...//
+
+                if (task.isSuccessful()) {
+
+//...then call requestLocationUpdates//
+
+                    requestLocationUpdates();
+                } else {
+
+//If sign in fails, then log the error//
+
+                    //Log.d(TAG, "Firebase authentication failed");
+                }
+            }
+        });
+    }
+
+//Initiate the request to track the device's location//
+
+    private void requestLocationUpdates() {
+        LocationRequest request = new LocationRequest();
+
+//Specify how often your app should request the device’s location//
+
+        request.setInterval(10);
+
+//Get the most accurate location data available//
+
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        final String path = getString(R.string.firebase_path);
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+//If the app currently has access to the location permission...//
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+
+//...then request location updates//
+
+            client.requestLocationUpdates(request, new LocationCallback() {
+
+     @Override
+     public void onLocationResult(LocationResult locationResult) {
+
+//Get a reference to the database, so your app can perform read and write operations//
+
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
+                    Location location = locationResult.getLastLocation();
+
+
+
+//Save the location data to the database//
+         String msg = Double.toString(location.getLatitude()) + "," +
+                 Double.toString(location.getLongitude());
+                        ref.setValue(msg);
+
+                        //ref.setValue(location.getLongitude());
+                        //ref.push();
+
+                }
+            }, null);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[]
+            grantResults) {
+
+//If the permission has been granted...//
+
+        if (requestCode == PERMISSIONS_REQUEST && grantResults.length == 1
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+//...then start the GPS tracking service//
+
+
+        } else {
+
+//If the user denies the permission request, then display a toast with some more information//
+
+            Toast.makeText(this, "Please enable location services to allow GPS tracking", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // to stop the listener and save battery
+        SensorManage.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Sensor sensor2;
+        // code for system's orientation sensor registered listeners
+        SensorManage.registerListener(this, SensorManage.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // get angle around the z-axis rotated
+        float degree = Math.round(event.values[0]);
+
+        DegreeTV.setText("Heading: " + Float.toString(degree) + " degrees");
+
+        // rotation animation - reverse turn degree degrees
+        RotateAnimation ra = new RotateAnimation(
+                DegreeStart,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+
+        // set the compass animation after the end of the reservation status
+        ra.setFillAfter(true);
+
+        // set how long the animation for the compass image will take place
+        ra.setDuration(210);
+
+        // Start animation of compass image
+        compassimage.startAnimation(ra);
+        DegreeStart = -degree;
+
+        final String path = getString(R.string.firebase_path2);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
+       /* String msg = "Compass:, " +
+                Double.toString(degree);*/
+        String msg = Double.toString(degree);
+        ref.setValue(msg);
+
+           // ref.setValue(degree);
+
+
+    }
+
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // not in use
+    }
+
+}
